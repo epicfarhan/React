038fe8273cd992bc000a471988960d5521df3ee7
Farhan <fnasim1102@gmail.com>
2025-06-05 02:52:02 +0600

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import { useState } from "react";
-import Create from "./components/Create";
-import TweetList from "./components/TweetList";
-
 function App() {
   // state
-  const [old, updated] = useState("");
-  const [tweet, setTweet] = useState([]);
 
-  return (
-    <>
-      <Create old={old} updated={updated} tweet={tweet} setTweet={setTweet} />
-      <TweetList tweet={tweet} setTweet={setTweet} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
